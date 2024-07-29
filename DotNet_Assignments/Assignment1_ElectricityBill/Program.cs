f9a2352721c50d8ae7e1dd8c652d12cdc570a316
MeghaS219 <meghasoman99@gmail.com>
2024-07-29 12:36:50 +0530

    Initial Commit

---
+++ DotNet_Assignments/Assignment1_ElectricityBill/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Assignment1_ElectricityBill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Double BillAmount;
+            Double Units;
+            while (true)
+            {
+                Console.WriteLine("Enter tye number of  Units Used");
+                if (Double.TryParse(Console.ReadLine(), out Units))
+                {
+                    if (Units > 250)
+                        BillAmount = (Units - 250) * 1.5 + 100 * (1.2 + 0.75) + 50 * 0.5;
+                    else if (Units > 150)
+                        BillAmount = (Units - 150) * 1.2 + 100 * 0.75 + 50 * 0.5;
+                    else if (Units > 50)
+                        BillAmount = (Units - 50) * 0.75 + 50 * 0.5;
+                    else
+                        BillAmount = Units * 0.5;
+                    Console.WriteLine("bill amount for " + Units + " units is " + BillAmount);
+                }
+                else
+                    Console.WriteLine("please enter a valid value for Units used");
+                Console.WriteLine("Do you want another check. Type Yes ore No");
+                string Choice = Console.ReadLine();
+                if (Choice != "yes")
+                {
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ DotNet_Assignments/Assignment2_StringOperations/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Assignment2_StringOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            operationThree(str);
+        }
+        static void operationOne(string str)
+        {
+            Console.WriteLine(str + str[0]);
+
+        }
+        static void operationTwo(string str)
+        {
+            string outstr="";
+            if (str.Length > 1)
+            {
+                outstr= str[str.Length - 1] + str.Substring(1, str.Length - 2) + str[0];
+
+            }
+            else
+                outstr = str;
+            Console.WriteLine(outstr);
+
+        }
+        static void operationThree(string str)
+        {
+            string outstr = "";
+            if (str.Length > 2)
+            {
+                outstr = str.Substring(0, 3) + str+ str.Substring(0, 3);
+
+            }
+            else
+                outstr = str+str+str;
+            Console.WriteLine(outstr);
+
+        }
+    }
+}
