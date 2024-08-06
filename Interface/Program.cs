0714caed6062b46ed116b78b69d69c69b4c09375
MeghaS219 <meghasoman99@gmail.com>
2024-08-06 15:04:58 +0530

    Added interface problem

---
+++ Interface/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gasolineRefuelAmount;
+            Car car = new Car(0);
+            Console.Write("Enter the amount of gasoline to refuel : ");
+            gasolineRefuelAmount = Convert.ToInt32(Console.ReadLine());
+            car.Refuel(gasolineRefuelAmount);
+            car.Drive();
+        }
+    }
+    
+    interface IVehiculo       
+    {            
+        public void Drive();            
+        public bool Refuel(int refuelamount);        
+    }       
+    class Car : IVehiculo        
+    {           
+        public int gasolineAmount;           
+        public Car(int startingGasolineAmount)            
+        {               
+            gasolineAmount = startingGasolineAmount;
+            
+        }           
+        public void Drive()           
+        {               
+            if (gasolineAmount > 0)               
+            {                   
+                Console.WriteLine("Driving");                
+            }
+            
+        }           
+        public bool Refuel(int refualamount)           
+        {         
+            gasolineAmount += refualamount;                
+            return true;            
+        }       
+    }      
+}
